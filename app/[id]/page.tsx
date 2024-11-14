85373d54f7d28cfc9d41b546862a9c5f28ffa95a
hajikhansahito110786 <hajikhansahito110786@gmail.com>
2024-11-14 11:57:30 +0500

    Add files via upload

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,15 +1,15 @@
 "use client";
-import { useParams,useSearchParams } from "next/navigation";
+//import { useParams,useSearchParams } from "next/navigation";
 
 const Page= () =>{
-  const usePathparameter=useParams();
-  console.log(usePathparameter);
+  //const usePathparameter=useParams();
+ //console.log(usePathparameter);
     //console.log(params)
-    const useSearchparameter=useSearchParams();
-    console.log(useSearchparameter);
-    const name=useSearchparameter.get("name")
-    const age=useSearchparameter.get("age")
-    console.log(name);
+   // const useSearchparameter=useSearchParams();
+   // console.log(useSearchparameter);
+   // const name=useSearchparameter.get("name")
+   // const age=useSearchparameter.get("age")
+   // console.log(name);
     //const {id}=await params;
     //const {name,age,edu}=await searchParams;
     //console.log(name,age,edu);
@@ -21,15 +21,14 @@ const Page= () =>{
        page
        </h1>
        <h1>
-       {usePathparameter.id}
+     
         
         </h1>
        <h1>
-       {name}
-       
+      
        </h1>
        <h1>
-       {age}
+       
         
        </h1>
        <h1>
--- app/page.tsx
+++ app/page.tsx
@@ -2,30 +2,31 @@
 
 "use client"
 
-import { useSearchParams } from "next/navigation";
+//import { useSearchParams } from "next/navigation";
+import { formAction } from "./Action/formData";
+
 export default function Home() {
 
-  const query=useSearchParams();
-  const name1=query.get("name1");
-  const id1=query.get("id");
+ // const query=useSearchParams();
+  //const name1=query.get("name1");
+  //const id1=query.get("id1");
 
   return (
     <div>
       home page
-      <form action={"/"}>
+      <form action={formAction}>
       
 <input type="text" name="name1" placeholder="data"  className="bg-slate-100 border-4 shadow-black m-5"/>
 
 <br/>
-<input type="number" name="id" placeholder="id"  className="bg-slate-100 border-4 shadow-black m-5"/>
+<input type="number" name="id1" placeholder="id"  className="bg-slate-100 border-4 shadow-black m-5"/>
 
 <input type="submit" name="submit" placeholder="submit"  className="bg-slate-100 border-4 shadow-black m-5"/>
 
       </form>
       <p>
 
-      {name1}
-      {id1}
+     
       </p>
 
         </div>
